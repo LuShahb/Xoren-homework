ef769f07a28de833e1b041e5994db9203b407a7d
LuShahb <shahbazyanlu@gmail.com>
2018-10-11 00:35:05 +0400

    Added Sherlock task to repo

---
+++ Sherlock_Homework/Count_Sherlock.py
@@ -0,0 +1,12 @@
+﻿import sys
+
+Sherlock = open(sys.argv[1], 'r')
+
+my_str = Sherlock.read()
+
+ 
+Sherlock.close()
+
+
+for i in "abcdefghijklmnopqrstuvwxz":
+    print("%s: %s" % (i, my_str.lower().count(i)))
